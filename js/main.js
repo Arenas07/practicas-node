6ede4169243b535c2e7dc4f7bbc6291ca6049e49
Arenas07 <189859279+Arenas07@users.noreply.github.com>
2025-03-28 14:41:26 +0100

    feat: :sparkles: funciones de edicion y guardado

--- js/main.js
+++ js/main.js
@@ -1,14 +1,3 @@
-const saveProduct = async (data) => {
-    
-    const config = {
-        method: "POST",
-        body: JSON.stringify(data)
-    };
-    const response = await fetch("http://localhost:5600/products", config)
-    const result = await response.json();
-    return result;
-    
-};
 
 // const editProduct = async(data) =>{
 //     const {id, ...dataUpdate} = data;
@@ -61,17 +50,140 @@ const saveProduct = async (data) => {
 // }
 
 const findProducts = async()=>{
+    const url = new URL("https://67e686946530dbd3111056dc.mockapi.io/")
+    url.pathname = "/products"
     const config = {
+
         method: "GET", //Obtener todos los productos
 
     }
-    const response = await fetch(`http://localhost:5600/products/`, config);
+    const response = await fetch(url, config)
     const result = await response.json();
     return result;
 } 
 
-while(confirm("¿Desea ver todos los productos?")){
-    findProducts().then(result => console.table((result)))
-    .catch(error => alert(error));
-    break;
+const dataProducts = await findProducts()
+console.table(dataProducts)
+
+const findUsers = async()=>{
+    const url = new URL("https://67e686946530dbd3111056dc.mockapi.io/")
+    url.pathname = "/users"
+    const config = {
+        method: "GET", //Obtener todos los productos
+
+    }
+    const response = await fetch(url, config)
+    const result = await response.json();
+    return result;
+} 
+
+const dataUsers = await findUsers()
+console.table(dataUsers)
+
+//******************************************************* */
+const saveUser = async (data) => {
+    const url = new URL("https://67e686946530dbd3111056dc.mockapi.io")
+    url.pathname = "/users"
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify(data)
+    };
+    const response = await fetch(url.toString(), config)
+    const result = await response.json();
+    return result;
+    
+};
+
+while(confirm("¿Desea insertar un dato para usuarios?")){
+    const data = {
+        name: prompt("Ingrese el nombre de usuario", "Santiago"),
+        last: prompt("Ingrese el apellido del usuario", "Arenas")
+    }
+
+    saveUser(data)
+}
+
+const saveProducts = async (data) => {
+    const url = new URL("https://67e686946530dbd3111056dc.mockapi.io")
+    url.pathname = "/products"
+    const header = new Headers()
+    header.append("Content-Type", "application/json")
+    const config = {
+        method: "POST",
+        headers: header,
+        body: JSON.stringify(data)
+    };
+    const response = await fetch(url.toString(), config)
+    const result = await response.json();
+    return result;
+    
+};
+
+while(confirm("¿Desea insertar un dato de productos?")){
+    const data = {
+        id: prompt("Ingrese el id", "6"),
+        name: prompt("Ingrese el nombre del producto", "Estufa"),
+        price: prompt("Ingrese el precio del producto", 500.99),
+        category: prompt("Ingrese la categoria del producto", "Electronics")
+    }
+    saveProducts(data)
+}
+
+//******************************************************/
+
+const editProduct = async(data) =>{
+    const { id, ...dataUpdate } = data;
+
+    const config = {
+        method: "PATCH",
+        headers: { 
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataUpdate)
+    };
+
+    const response = await fetch(`https://67e686946530dbd3111056dc.mockapi.io/products/${id}`, config);
+    const result = await response.json();
+    return result;
+ }
+
+ while (confirm("¿Desea actualizar un producto?")) {
+    const data = {
+        id: prompt("Ingrese el id a editar"),
+        name: prompt("Ingrese el nombre del producto a editar"),
+        price: prompt("Ingrese el precio del producto a editar"),
+        category: prompt("Ingrese la categoría del producto")
+    };
+
+    await editProduct(data);
+}
+
+
+const editUsers = async(data) =>{
+    const { id, ...dataUpdate } = data;
+
+    const config = {
+        method: "PATCH",
+        headers: { 
+            "Content-Type": "application/json"
+        },
+        body: JSON.stringify(dataUpdate)
+    };
+
+    const response = await fetch(`https://67e686946530dbd3111056dc.mockapi.io/users/${id}`, config);
+    const result = await response.json();
+    return result;
+ }
+
+ while (confirm("¿Desea actualizar un usuario?")) {
+    const data = {
+        id: prompt("Ingrese el id a editar"),
+        name: prompt("Ingrese el nombre del usuario a editar"),
+        last: prompt("Ingrese el precio del usuario a editar"),
+        
+    };
+    await editProduct(data);
 }
